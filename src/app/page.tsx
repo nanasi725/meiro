4dd9a2e5d807c754db8d58aeedc4424dc7cea243
nanasi725 <s1f102501787@iniad.org>
2025-06-21 00:06:46 +0900

    'マップ作製'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,9 @@
 import styles from './page.module.css';
 
 export default function Home() {
-  return <div className={styles.container}>a</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.map} />
+    </div>
+  );
 }
